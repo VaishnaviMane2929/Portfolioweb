956ad77a98cf8ffa3e3be3ac0f349b9ea6f16de6
vaishnavi <vaishnavimane991@gmail.com>
2024-12-29 23:20:24 +0800

    set the image

--- src/Componantes/nav/Nav.jsx
+++ src/Componantes/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { TiHomeOutline } from "react-icons/ti";
+import { FaRegUser } from "react-icons/fa";
+import { RiBook2Line } from "react-icons/ri";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageAltDetail } from "react-icons/bi";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+   <nav>
+    <a href="# home"><TiHomeOutline/></a>
+    <a href="# about"><FaRegUser/> </a>
+    <a href="# experience"><RiBook2Line/></a>
+    <a href="# services"><RiServiceLine/></a>
+    <a href="# contact"><BiMessageAltDetail/></a>
+   </nav>
   )
 }
 
