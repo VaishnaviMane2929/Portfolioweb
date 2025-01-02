82eb389d456bcc94fa1c7613b58ede7699188525
vaishnavi <vaishnavimane991@gmail.com>
2025-01-02 10:41:50 +0530

    portfolio section

--- src/Componantes/portfolio/Portfolio.jsx
+++ src/Componantes/portfolio/Portfolio.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from 
+import IMG1 from'../../assets/portfolio1.png'
+import IMG2 from'../../assets/portfolio.4png.jpg'
+import IMG3 from'../../assets/portfolio3.png'
+import IMG4 from'../../assets/portfolio5.png'
+import IMG5 from'../../assets/portfolio7.png'
+import IMG6 from'../../assets/portfolio6.png'
+
+
 
 const Portfolio = () => {
   return (
@@ -9,11 +16,11 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio_container'>
-        <article>
+        <article className='portfolio_item'>
           <div className='portfolio_item-image'>
 
             <h3>This is a portfolio item title</h3>
-            <a href="https://github.com/VaishnaviMane2929" className='btn'>GitHub</a>
+            <a href="https://github.com/VaishnaviMane2929" className='btn' target='_blank'>GitHub</a>
             <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
 
           </div>
