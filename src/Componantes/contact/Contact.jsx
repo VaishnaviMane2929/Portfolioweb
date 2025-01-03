7ad922c2abfeeecffb799ef6765f9cdc5ec4a7ed
vaishnavi <vaishnavimane991@gmail.com>
2025-01-03 21:19:18 +0530

    footer section

--- src/Componantes/contact/Contact.jsx
+++ src/Componantes/contact/Contact.jsx
@@ -1,52 +1,101 @@
-import React from 'react'
-import './contact.css'
-import { MdOutlineMail } from "react-icons/md";
-import { RiMessengerLine } from "react-icons/ri";
-import { FaWhatsapp } from "react-icons/fa6";
-
-
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { useRef, useState } from "react";
+// import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+  //   emailjs
+  //     .sendForm(
+  //       "service_1th6yd9",
+  //       "template_13m1fsf",
+  //       form.current,
+  //       "oN2M8beRG-WaZIk0_"
+  //     )
+  //     .then((response) => {
+  //       console.log("Success!", response.status, response.text);
+  //       setMessage("Your message has been sent successfully!");
+  //       setMessageType("success");
+  //       form.current.reset();
+  //     })
+  //     .catch((err) => {
+  //       console.log("Failed...", err);
+  //     });
+  };
   return (
-    <section id='contact'>
-      <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
-      <div className='container contact_container'>
-        <div className='contact_option'>
-          <article className='contact_option'>
-            <MdOutlineMail className='contact_option-icon'/>
-            <h4>Email</h4>
-            <h5>vaishu991@gmail.com</h5>
-            <a href="mailto:welcome@gmail.com"target='_blank'>Send Message</a>
+    <>
+      <section id="contact">
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
 
-          </article>
-          <article className='contact_option'>
-            <RiMessengerLine className='contact_option-icon'/>
-            <h4>Messenger</h4>
-            <h5>egatotutorials</h5>
-            <a href="https://www.instagram.com/" target='_blank'>Send Message</a>
-
-          </article>
-          <article className='contact_option'>
-            <FaWhatsapp className='contact_option-icon'/>
-            <h4>Messenger</h4>
-            <h5>+9699978033</h5>
-            <a href="https://api.whatsapp.com/send?phone+9699978033"target='_blank'>Send Message</a>
-
-          </article>
+        <div className="container contact_container">
+          <div className="contact_options">
+            <article className="contact_option">
+              <MdOutlineEmail className="contact_option_icon" />
+              <h4>Email</h4>
+              <h5>vaishnavimane@gmail.com</h5>
+              <a href="mailto:vaishnavimane991@gmail.com">Send a Message</a>
+            </article>
+            <article className="contact_option">
+              <FaLinkedin className="contact_option_icon" />
+              <h4>LinkedIn</h4>
+              <h5>Vaishnavi Mane</h5>
+              <a href="https://www.linkedin.com/in/vaishnavi-mane-a415a633a/">Send a Message</a>
+            </article>
+            <article className="contact_option">
+              <FaWhatsapp className="contact_option_icon" />
+              <h4>WhatsApp</h4>
+              <h5>Vaishnavi Mane</h5>
+              <a href="https://api.whatsapp.com/send/?phone+9699978033">
+                Send a Message
+              </a>
+            </article>
+          </div>
+          {/* end of contact option  */} 
+           <form ref={form} onSubmit={sendEmail}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Full Name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
+            <textarea
+              name="message"
+              rows={7}
+              placeholder="Your Message"
+              required
+            ></textarea>
+            <button type="submit" className="btn btn-primary" value="Send">
+              Send Message
+            </button>
+            {message && (
+              <div
+                className={`alert ${
+                  messageType === "success" ? "alert-success" : "alert-error"
+                }`}
+              >
+                {message}
+              </div>
+            )}
+          </form>
         </div>
-        {/* ==========end of contact option========== */}
-        <form action="">
-          <input type="text" name='Name' placeholder='Your Full Name:' required/>
-          <input type="email" name='email' placeholder='Your Email' required />
-          <textarea name="Message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='Submit' className='btn btn-primary'>Send Message</button>
+      </section>
+    </>
+  );
+};
 
-        </form>
-
-      </div>
-    </section>
-  )
-}
-
-export default Contact
+export default Contact;
--- src/Componantes/footer/Footer.jsx
+++ src/Componantes/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
+import { GrInstagram } from "react-icons/gr";
+import { SiGithub } from "react-icons/si";
+import { FaLinkedin } from "react-icons/fa6";
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'></a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div className='footer_socials'>
+        <a href="https://www.instagram.com/vaishnavi_mane242/"><GrInstagram/></a>
+        <a href="https://github.com/VaishnaviMane2929"><SiGithub/></a>
+        <a href="https://www.linkedin.com/in/vaishnavi-mane-a415a633a/"><FaLinkedin/></a>
+      </div>
+      <div className='footer_socials'>
+        <small>&copy; EGATOR Tutorials. All right reserved</small>
+
+
+      </div>
+    </footer>
   )
 }
 
